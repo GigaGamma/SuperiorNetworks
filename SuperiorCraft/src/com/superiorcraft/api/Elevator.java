73e2a66013456e8e96ecc1e1b30b03513ae1cb98
Auguste Rame <auguste.rame@gmail.com>
2017-04-29 12:49:53 -0400

    Elevate

--- SuperiorCraft/src/com/superiorcraft/api/Elevator.java
+++ SuperiorCraft/src/com/superiorcraft/api/Elevator.java
@@ -1,5 +1,8 @@
 package com.superiorcraft.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
@@ -21,13 +24,54 @@ public class Elevator extends CustomBlockLoader {
 		return true;
 	}
 	
+	public List<Entity> getConnectedElevator(Entity e, Player p) {
+		ArrayList<Entity> ents = new ArrayList<Entity>();
+		
+		for (Entity en : p.getWorld().getEntities()) {
+			if (en != e && en.getCustomName() != null && en.getCustomName().equals(name) && en.getLocation().getX() == e.getLocation().getX() && en.getLocation().getZ() == e.getLocation().getZ()) {
+				ents.add(en);
+			}
+		}
+		
+		return ents;
+	}
+	
+	public Entity getElevatorUp(Entity e, Player p) {
+		double bd = Double.MAX_VALUE;
+		Entity be = null;
+		
+		for (Entity en : getConnectedElevator(e, p)) {
+			if (e.getLocation().getY() < en.getLocation().getY() && e.getLocation().distance(en.getLocation()) < bd) {
+				bd = e.getLocation().distance(en.getLocation());
+				be = en;
+				//p.sendMessage(String.valueOf(en.getLocation().getY() + " : " + String.valueOf(e.getLocation().distance(en.getLocation()))));
+			}
+		}
+		
+		p.sendMessage(String.valueOf(be.getLocation().getY()));
+		return be;
+	}
+	
+	public Entity getElevatorDown(Entity e, Player p) {
+		double bd = Double.MAX_VALUE;
+		Entity be = null;
+		
+		for (Entity en : getConnectedElevator(e, p)) {
+			if (e.getLocation().getY() > en.getLocation().getY() && e.getLocation().distance(en.getLocation()) < bd) {
+				bd = e.getLocation().distance(en.getLocation());
+				be = en;
+			}
+		}
+		
+		return be;
+	}
+	
 	@EventHandler
 	public void onPlayerMoveEvent(PlayerMoveEvent e) {
 		for (Entity en : e.getPlayer().getWorld().getEntities()) {
 			if (en.getCustomName() != null && en.getCustomName().equals(name) && en.getLocation().distance(e.getTo()) <= 1.5 && en.getLocation().getY() < e.getTo().getY()) {
 				if (e.getTo().getY() - en.getLocation().getY() >= 1.2) {
-					e.getPlayer().sendMessage("up");
-					e.setCancelled(true);
+					if (getElevatorUp(en, e.getPlayer()) != null) {e.setTo(getElevatorUp(en, e.getPlayer()).getLocation().add(0, 1, 0));}
 				}
 			}
 		}
@@ -40,7 +84,8 @@ public class Elevator extends CustomBlockLoader {
 		for (Entity en : e.getPlayer().getWorld().getEntities()) {
 			if (en.getCustomName() != null && en.getCustomName().equals(name) && en.getLocation().distance(e.getPlayer().getLocation()) <= 1.5 && en.getLocation().getY() < e.getPlayer().getLocation().getY()) {
 				if (!e.getPlayer().isSneaking()) {
-					e.getPlayer().sendMessage("down");
+					//e.getPlayer().sendMessage("down");
+					if (getElevatorDown(en, e.getPlayer()) != null) {e.getPlayer().teleport(getElevatorDown(en, e.getPlayer()).getLocation().add(0, 1, 0));}
 				}
 			}
 		}
